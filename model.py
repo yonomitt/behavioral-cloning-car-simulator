e65035e1446fae5f5eda69473f94510fc16c2d7f
Yonatan Mittlefehldt <yono@toojuice.com>
2017-03-15 22:30:32 +0100

    Added a version of conv_3_fc_3 that has 4 times as many filters

--- model.py
+++ model.py
@@ -34,6 +34,69 @@ STEERING_CORRECTION = 0.2
 VALIDATION_PCT = 0.2
 
 
+def conv_3_fc_3_more_filters(dropout = []):
+
+    """This network has three convolution layers and three fully connected layers
+
+    Parameters:
+        dropout - list of dropout values for the 3 fully connected layers"""
+
+    params = {
+        'conv1': { 'filters': 8,  'size': 5 },
+        'conv2': { 'filters': 16, 'size': 3 },
+        'conv3': { 'filters': 32, 'size': 3 },
+        'full4': { 'outputs': 556 },
+        'full5': { 'outputs': 24 },
+    }
+
+    if dropout == None or len(dropout) == 0:
+        dropout = [0.0, 0.0]
+    elif len(dropout) == 1:
+        dropout = dropout * 2
+
+    # this hack gets the current function name and sets it to the name of the model
+    model = Sequential(name=traceback.extract_stack(None, 2)[-1][2])
+
+    # crop top 28 rows and bottom 12 rows from the images
+    model.add(Cropping2D(cropping=((28, 12), (0, 0)), input_shape=(80, 160, 3), name='pp_crop'))
+
+    # mean center the pixels
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, name='pp_center'))
+
+    # layer 1: convolution + max pooling. Input 40x160x3. Output 20x80x32
+    model.add(Convolution2D(32, 5, 5, border_mode='same', name='conv1'))
+    model.add(MaxPooling2D((2, 2), name='pool1'))
+    model.add(Activation('relu', name='act1'))
+
+    # layer 2: convolution = max pooling. Input 20x80x32. Output 10x40x64
+    model.add(Convolution2D(64, 3, 3, border_mode='same', name='conv2'))
+    model.add(MaxPooling2D((2, 2), name='pool2'))
+    model.add(Activation('relu', name='act2'))
+
+    # layer 3: convolution = max pooling. Input 10x40x64. Output 5x20x128
+    model.add(Convolution2D(128, 3, 3, border_mode='same', name='conv3'))
+    model.add(MaxPooling2D((2, 2), name='pool3'))
+    model.add(Activation('relu', name='act3'))
+
+    # flatten: Input 5x20x128. Output 12800
+    model.add(Flatten(name='flat'))
+
+    # layer 4: fully connected + dropout. Input 12800. Output 556
+    model.add(Dense(556, name='fc4'))
+    model.add(Dropout(dropout[0], name='drop4'))
+    model.add(Activation('relu', name='act4'))
+
+    # layer 5: fully connected + dropout. Input 556. Output 24
+    model.add(Dense(24, name='fc5'))
+    model.add(Dropout(dropout[1], name='drop5'))
+    model.add(Activation('relu', name='act5'))
+
+    # layer 6: fully connected. Input 24. Output 1.
+    model.add(Dense(1, name='out'))
+
+    return model
+
+
 def conv_3_fc_3(dropout = []):
 
     """This network has three convolution layers and three fully connected layers
